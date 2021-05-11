df490356297ad03d05adf053ea866cdeb091bbe8
barryz <barryzxb@gmail.com>
2021-05-11 09:41:20 +0800

    tx_trace: incr tx reexec timeout to 30s

--- ethapi/tx_trace.go
+++ ethapi/tx_trace.go
@@ -411,7 +411,7 @@ func getStructLogForTransaction(
 	// Setup context so it may be cancelled the call has completed
 	// or, in case of unmetered gas, setup a context with a timeout.
 	// TODO add time into the server configuration
-	var timeout time.Duration = 2 * time.Second
+	var timeout time.Duration = 30 * time.Second
 	var cancel context.CancelFunc
 	ctx, cancel = context.WithTimeout(ctx, timeout)
 
